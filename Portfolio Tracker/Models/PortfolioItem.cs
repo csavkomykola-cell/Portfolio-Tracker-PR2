bedfb2be421189a5cb192dae8c081771739956ef
csavkomykola-cell <c.savko.mykola@student.uzhnu.edu.ua>
2026-05-14 19:21:12 +0300

    Automatic Portfolio Calculation Added
    (Will Be Improved)

--- Portfolio Tracker/Models/PortfolioItem.cs
+++ Portfolio Tracker/Models/PortfolioItem.cs
@@ -1,19 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Portfolio_Tracker.Models
 {
-    public class PortfolioItem
+    public class PortfolioItem : INotifyPropertyChanged
     {
-        public string Symbol { get; set; }
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public decimal AvgPrice { get; set; }
-        public decimal CurrentPrice { get; set; }
-        public decimal TotalValue => (decimal)Quantity * CurrentPrice;
-        public decimal Profit => TotalValue - ((decimal)Quantity * AvgPrice);
+        private string _symbol;
+        public string Symbol
+        {
+            get => _symbol;
+            set => SetProperty(ref _symbol, value);
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        private double _quantity;
+        public double Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (SetProperty(ref _quantity, value))
+                    OnPropertyChanged(nameof(CurrentValue));
+            }
+        }
+
+        private decimal _averagePrice;
+        public decimal AveragePrice
+        {
+            get => _averagePrice;
+            set
+            {
+                if (SetProperty(ref _averagePrice, value))
+                    OnPropertyChanged(nameof(CurrentValue));
+            }
+        }
+
+        private decimal _currentPrice;
+        public decimal CurrentPrice
+        {
+            get => _currentPrice;
+            set
+            {
+                if (SetProperty(ref _currentPrice, value))
+                    OnPropertyChanged(nameof(CurrentValue));
+            }
+        }
+
+        public decimal CurrentValue => (decimal)Quantity * CurrentPrice;
+
+        public decimal ProfitLoss => CurrentValue - (decimal)Quantity * AveragePrice;
+
+        private string _currency;
+        public string Currency
+        {
+            get => _currency;
+            set => SetProperty(ref _currency, value);
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected bool SetProperty<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(backingField, value))
+                return false;
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
     }
 }
--- Portfolio Tracker/ViewModels/PortfolioViewModel.cs
+++ Portfolio Tracker/ViewModels/PortfolioViewModel.cs
@@ -1,46 +1,155 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Portfolio_Tracker.Models;
+﻿using Portfolio_Tracker.Models;
+using Portfolio_Tracker.Services;
+using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Portfolio_Tracker.ViewModels
 {
-    public class PortfolioViewModel
+    public class PortfolioViewModel : INotifyPropertyChanged, IDisposable
     {
-        public ObservableCollection<PortfolioItem> PortfolioItems { get; set; }
+        private const string TransactionsPath = "Data/transactions.json";
+        private const string AssetsPath = "Data/assets.json";
+
+        public ObservableCollection<PortfolioItem> Portfolio { get; set; } = new ObservableCollection<PortfolioItem>();
+
+        private decimal _totalValue;
+        public decimal TotalValue
+        {
+            get => _totalValue;
+            private set { _totalValue = value; OnPropertyChanged(nameof(TotalValue)); }
+        }
+
+        private decimal _totalProfitLoss;
+        public decimal TotalProfitLoss
+        {
+            get => _totalProfitLoss;
+            private set { _totalProfitLoss = value; OnPropertyChanged(nameof(TotalProfitLoss)); }
+        }
+
+        private int _assetCount;
+        public int AssetCount
+        {
+            get => _assetCount;
+            private set { _assetCount = value; OnPropertyChanged(nameof(AssetCount)); }
+        }
+
+        private FileSystemWatcher _watcher;
 
         public PortfolioViewModel()
         {
-            PortfolioItems = new ObservableCollection<PortfolioItem>
+            Recalculate();
+
+            // Відстежувати файл транзакцій, щоб зміни (збереження) автоматично оновлювали портфель
+            try
             {
-                new PortfolioItem
+                var dataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+                if (!Directory.Exists(dataDir)) Directory.CreateDirectory(dataDir);
+
+                _watcher = new FileSystemWatcher(dataDir, Path.GetFileName(TransactionsPath))
                 {
-                    Symbol="AAPL",
-                    Name="Apple",
-                    Quantity=40,
-                    AvgPrice=450,
-                    CurrentPrice=480
-                },
-                new PortfolioItem
+                    NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size
+                };
+                _watcher.Changed += OnTransactionsFileChanged;
+                _watcher.EnableRaisingEvents = true;
+            }
+            catch
+            {
+                // ігнорувати помилки спостерігача
+            }
+        }
+
+        private void OnTransactionsFileChanged(object sender, FileSystemEventArgs e)
+        {
+            // файл може бути заблокований на короткий час; затримка та виклик на UI-потік
+            Application.Current?.Dispatcher?.BeginInvoke(new Action(() =>
+            {
+                try
                 {
-                    Symbol="BTC",
-                    Name="Bitcoin",
-                    Quantity=0.4,
-                    AvgPrice=40000,
-                    CurrentPrice=44000
-                },
-                new PortfolioItem
-                {
-                    Symbol="TSLA",
-                    Name="Tesla",
-                    Quantity=4,
-                    AvgPrice=440,
-                    CurrentPrice=4400
+                    Recalculate();
                 }
-            };
+                catch
+                {
+                    // ігнорувати тимчасові помилки читання
+                }
+            }), DispatcherPriority.Background);
+        }
+
+        public void Recalculate()
+        {
+            // Завантажити транзакції та активи
+            var transactions = JsonService.LoadTransactions<ObservableCollection<Transaction>>() ?? new ObservableCollection<Transaction>();
+            var assets = JsonService.LoadAssets<ObservableCollection<Asset>>() ?? new ObservableCollection<Asset>();
+
+            // Групувати транзакції за символом активу
+            var groups = transactions
+                .GroupBy(t => t.Asset?.Trim().ToUpper() ?? string.Empty)
+                .Where(g => !string.IsNullOrEmpty(g.Key));
+
+            var newPortfolio = groups.Select(g =>
+            {
+                var symbol = g.Key;
+                var name = g.FirstOrDefault()?.Asset ?? symbol;
+
+                // Підсумувати покупки та продажі окремо
+                double buyQty = g.Where(t => string.Equals(t.Type, "Buy", StringComparison.OrdinalIgnoreCase)).Sum(t => t.Quantity);
+                decimal buyCost = g.Where(t => string.Equals(t.Type, "Buy", StringComparison.OrdinalIgnoreCase))
+                                    .Sum(t => (decimal)t.Quantity * t.Price + t.Fees);
+
+                double sellQty = g.Where(t => string.Equals(t.Type, "Sell", StringComparison.OrdinalIgnoreCase)).Sum(t => t.Quantity);
+                // Ми не коригуємо базу собівартості для продажів для розрахунку нереалізованого прибутку/збитку тут
+
+                double netQuantity = buyQty - sellQty;
+
+                decimal avgPrice = 0m;
+                if (buyQty > 0)
+                    avgPrice = buyCost / (decimal)buyQty;
+
+                var asset = assets.FirstOrDefault(a => string.Equals(a.Symbol, symbol, StringComparison.OrdinalIgnoreCase));
+                decimal currentPrice = asset?.CurrentPrice ?? 0m;
+                string currency = asset?.Currency ?? "USD";
+
+                return new PortfolioItem
+                {
+                    Symbol = symbol,
+                    Name = asset?.Name ?? name,
+                    Quantity = netQuantity,
+                    AveragePrice = avgPrice,
+                    CurrentPrice = currentPrice,
+                    Currency = currency
+                };
+            })
+            // Зберігати лише позитивні позиції
+            .Where(p => p.Quantity > 0)
+            .OrderBy(p => p.Symbol)
+            .ToList();
+
+            // Оновити спостережувану колекцію
+            Portfolio.Clear();
+            foreach (var item in newPortfolio)
+                Portfolio.Add(item);
+
+            // Оновити підсумки
+            TotalValue = Portfolio.Sum(p => p.CurrentValue);
+            TotalProfitLoss = Portfolio.Sum(p => p.ProfitLoss);
+            AssetCount = Portfolio.Count;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public void Dispose()
+        {
+            if (_watcher != null)
+            {
+                _watcher.EnableRaisingEvents = false;
+                _watcher.Dispose();
+                _watcher = null;
+            }
         }
     }
 }
